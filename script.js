4cd1a62d54288fc95894e9728ff4aca64ae08b22
nngocjade <nngocjade@gmail.com>
2020-12-21 17:09:10 +0700

    done with assignment 1

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function getAverage() {
 }
 console.log(getAverage());
 
-//~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function getAddress() {
   let coderschool = {
@@ -47,4 +47,53 @@ function getAddress() {
   return city === "HCMC" && country === "Vietnam" && street === "Ton Dan";
 }
 console.log(getAddress());
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function getElements() {
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  // refactor the lines below
+  // const first = arr[0];
+  // const third = arr[2];
+  // const fourth = arr[4];
+  // YOUR CODE STARTS HERE
+  const [first, , third, fourth] = [0, , 1, 2];
+  console.group("getElements();");
+  console.log("const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];");
+  console.log("const [first, , third, fourth] = [0, , 1, 2];");
+  console.log("return { first, third, fourth };");
+  return { first, third, fourth };
+}
+console.log(getElements()); //to show the return values of return { first, third, fourth };
+console.groupEnd();
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function getNestedElements() {
+  const food = [
+    ["carrots", "beans", "peas", "lettuce"],
+    ["apples", "mangos", "oranges"],
+    ["cookies", "cake", "pizza", "chocolate"],
+  ];
+  // refactor the lines below
+  // const carrots = food[0][0];
+  // const cookies = food[2][0];
+  // const mangos = food[1][1];
+  // YOUR CODE STARTS HERE
+
+  const [[carrots], [, mangos], [cookies]] = food;
+  console.group("getNestedElements();");
+  console.log(
+    ` const food = [
+      ["carrots", "beans", "peas", "lettuce"],
+      ["apples", "mangos", "oranges"],
+      ["cookies", "cake", "pizza", "chocolate"],
+    ];`
+  );
+  console.log(`const [[carrots],[ , mangos],[cookies]] = food;`);
+  console.log(` return { carrots, cookies, mangos };`);
+  return { carrots, cookies, mangos };
+}
+
+console.log(getNestedElements());
 console.groupEnd();
